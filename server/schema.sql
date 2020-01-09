35d5133c80a94f82d6c61cf60f35f89f44f1fc93
almostgone47 <almostgone47@hotmail.com>
2020-01-09 15:18:39 -0800

    schema complete

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,45 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  body VARCHAR(255),
+  user_id int,
+    INDEX user_ind (user_id),
+    FOREIGN KEY (user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE,
+  room_id int,
+    INDEX room_ind (room_id),
+    FOREIGN KEY (room_id)
+      REFERENCES rooms(id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE rooms_users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id int,
+    INDEX user_ind (user_id),
+    FOREIGN KEY (user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE,
+  room_id int,
+    INDEX room_ind (room_id),
+    FOREIGN KEY (room_id)
+      REFERENCES rooms(id)
+      ON DELETE CASCADE
 );
 
 /* Create other tables and define schemas for them here! */
@@ -12,6 +48,6 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
